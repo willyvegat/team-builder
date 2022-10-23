58eaa0825f67b8a401df5740251e78876dca5788
Willy Vega <willy.vegaa@Willys-MacBook-Pro-2.local>
2022-10-23 18:16:08 -0400

    created form with three inputs, handleChange and handleSubmit

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,9 +1,44 @@
 import React from 'react';
 
 const Form = (props) => {
-    return (
-        <form>
+    
+    const handleChange = event => {
+        const { name, value } = event.target;
+        props.change(name, value);
+    }
 
+    const handleSubmit = event => {
+        event.preventDefault();
+        props.submit();
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>Name: 
+                <input
+                    placeholder='Enter your name here!'
+                    value={values.name}
+                    name="name"
+                    onChange={handleChange}
+                />
+            </label>    
+            <label>Email: 
+                <input 
+                    placeholder='Enter your email here!'
+                    value={values.email}
+                    name="email"
+                    onChange={handleChange}
+                />
+            </label>
+            <label>Role: 
+                <input 
+                    placeholder='Enter your role here!'
+                    value={values.role}
+                    name="role"
+                    onChange={handleChange}
+                />
+            </label>
+            <input type='submit' value="Create your team!" />
         </form>
     )
 }
